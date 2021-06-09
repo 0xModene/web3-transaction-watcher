07a550a5daafc93f816585a8637eb01be323cabd
Zachary Parrish <zachary@parrish.io>
2021-06-09 19:26:07 -0400

    initial commit, copied over code from index-ui

---
+++ src/lib/components/TransactionWatcher/TransactionWatcher.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import PendingTransaction from "./PendingTransaction";
+import ApprovingTransaction from "./ApprovingTransaction";
+import CompletedTransaction from "./CompletedTransaction";
+import FailedTransaction from "./FailedTransaction";
+import LongTransaction from "./LongTransaction";
+
+export enum TransactionStatusType {
+  IS_UNSTARTED,
+  IS_APPROVING,
+  IS_PENDING,
+  IS_COMPLETED,
+  IS_FAILED,
+  IS_LONG,
+}
+
+interface TransactionWatcherProps {
+  transactionStatus?: TransactionStatusType;
+  startTransactionComponent?: any;
+  pendingTransactionComponent?: any;
+  approvingTransactionComponent?: any;
+  completedTransactionComponent?: any;
+  failedTransactionComponent?: any;
+  longTransactionComponent?: any;
+}
+
+const ConfirmTransactionModal: React.FC<TransactionWatcherProps> = ({
+  transactionStatus,
+  startTransactionComponent,
+  approvingTransactionComponent = <ApprovingTransaction />,
+  pendingTransactionComponent = <PendingTransaction />,
+  completedTransactionComponent = <CompletedTransaction />,
+  failedTransactionComponent = <FailedTransaction />,
+  longTransactionComponent = <LongTransaction />,
+}) => {
+  switch (transactionStatus) {
+    case TransactionStatusType.IS_UNSTARTED:
+      return startTransactionComponent;
+
+    case TransactionStatusType.IS_APPROVING:
+      return approvingTransactionComponent;
+
+    case TransactionStatusType.IS_PENDING:
+      return pendingTransactionComponent;
+
+    case TransactionStatusType.IS_COMPLETED:
+      return completedTransactionComponent;
+
+    case TransactionStatusType.IS_FAILED:
+      return failedTransactionComponent;
+
+    case TransactionStatusType.IS_LONG:
+      return longTransactionComponent;
+
+    default:
+      return null;
+  }
+};
+
+export default ConfirmTransactionModal;

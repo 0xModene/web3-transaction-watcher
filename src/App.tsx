99045d741ee5d262cd530ca5241c6dffdfae9a89
Zachary Parrish <zachary@parrish.io>
2021-06-11 20:50:22 -0400

    App.tsx added

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import TransactionWatcher from "lib/components/TransactionWatcher";
+import useTransactionWatcher from "lib/hooks/useTransactionWatcher";
+import React from "react";
+
+const App: React.FC = () => {
+  return <TransactionWatcher></TransactionWatcher>;
+};
+
+export default App;
